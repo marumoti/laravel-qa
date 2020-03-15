14771baadafb5256f0a81883188f889bf87b7012
marumoti <ukn33@yahoo.co.jp>
2020-03-15 18:19:54 +0900

    投票の追加、見られた回数　質問状態の色設定

--- app/Question.php
+++ app/Question.php
@@ -9,25 +9,37 @@ use Illuminate\Support\Str;
 
 class Question extends Model
 {
-    protected $fillable = ['title','body']; //指定した属性しか持たない
+    protected $fillable = ['title', 'body']; //指定した属性しか持たない
 
-    public function user(){ //質問はユーザに属する
+    public function user()
+    { //質問はユーザに属する
         return $this->belongsTo(User::class); //1対多
     }
 
-   public function setTitleAttribute($value)
-   {
-    $this->attributes['title'] = $value; //タイトル属性に値を割り当てる
-    $this->attributes['slug'] = Str::slug($value); //スラッグ形式に変換
-   }
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value; //タイトル属性に値を割り当てる
+        $this->attributes['slug'] = Str::slug($value); //スラッグ形式に変換
+    }
 
-   public function getUrlAttribute()
-   {
-       return route("questions.show",$this->id);
-   }
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
 
-   public function getCreatedDateAttribute()
-   {
-       return $this->created_at->diffForHumans();
-   }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()     //質問の状態を返す
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
