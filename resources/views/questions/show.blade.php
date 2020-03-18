04667b7eee6ea3b2aa61b2db2a693523ebd7ca77
marumoti <ukn33@yahoo.co.jp>
2020-03-18 15:04:48 +0900

    質問と回答の欄に投票システムの作成

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,63 +5,92 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h1>{{ $question->title }}</h1>
-                        <div class="ml-auto">
-                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">全ての質問に戻る</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card-body">
-                    {!! $question->body_html !!}
-                    <div class="float-right">
-                        <span class="text-muted">Answerd {{ $question->created_date }}</span>
-                         <div class="media mt-2">
-                            <a href="{{ $question->user->url }}" class="pr-2">
-                             <img src="{{ $question->user->avatar }}">
-                            </a>
-                            <div class="media-body mt-1">
-                              <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                             </div>
-                        </div>
-                    </div>
-                    <!-- マークダウンをHTMLに変換 -->
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mt-4">
-        <div class="col-md-12">
-            <div class="card">
                 <div class="card-body">
                     <div class="card-title">
-                        <h2>{{ $question->answers_count . " ". str_plural('Answer',$question->answers_count) }}</h2>
-                    </div>
-                    <hr>
-                    @foreach($question->answers as $answer)
-                    <div class="media">
-                        <div class="media-body">
-                            {!! $answer->body_html !!}
-                            <div class="float-right">
-                                <span class="text-muted">Answerd {{ $answer->created_date }}</span>
-                                <div class="media mt-2">
-                                    <a href="{{ $answer->user->url }}" class="pr-2">
-                                        <img src="{{ $answer->user->avatar }}">
-                                    </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                    </div>
-                                </div>
+                        <div class="d-flex align-items-center">
+                            <h1>{{ $question->title }}</h1>
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">全ての質問に戻る</a>
                             </div>
                         </div>
                     </div>
                     <hr>
-                    @endforeach
+
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a title="This question is useful" class="vote-up">
+                                <i class="fa fa-caret-up fa-3x"></i>
+                            </a>
+                            <span class="votes-count">1230</span>
+                            <a title="This question is not useful" class="vote-down off">
+                                <i class="fa fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="クリックしてお気に入り登録する(もう一度クリックを押すと元に戻します)" class="favorite mt-2 favorited">
+                                <i class="fa fa-star fa-2x"></i>
+                                <span class="favorites-count">123</span>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">Answerd {{ $question->created_date }}</span>
+                                <div class="media mt-2">
+                                    <a href="{{ $question->user->url }}" class="pr-2">
+                                        <img src="{{ $question->user->avatar }}">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- マークダウンをHTMLに変換 -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h2>{{ $question->answers_count . " ". str_plural('Answer',$question->answers_count) }}</h2>
+                        </div>
+                        <hr>
+                        @foreach($question->answers as $answer)
+                        <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a title="This answer is useful" class="vote-up">
+                                    <i class="fa fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">1230</span>
+                                <a title="This answer is not useful" class="vote-down off">
+                                    <i class="fa fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="この回答をベストアンサーとしてマークする" class="vote-accepted mt-2">
+                                    <i class="fa fa-check fa-2x"></i>
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                {!! $answer->body_html !!}
+                                <div class="float-right">
+                                    <span class="text-muted">Answerd {{ $answer->created_date }}</span>
+                                    <div class="media mt-2">
+                                        <a href="{{ $answer->user->url }}" class="pr-2">
+                                            <img src="{{ $answer->user->avatar }}">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
