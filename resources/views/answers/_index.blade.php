992c878b17e96941b455fca56515e199beb0f6a9
marumoti <ukn33@yahoo.co.jp>
2020-04-05 16:41:54 +0900

    ビューの修正

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -10,69 +10,7 @@
                 @include('layouts._messages')
 
                 @foreach($answers as $answer)
-                <div class="media">
-                    <div class="d-flex flex-column vote-controls">
-                        <a title="This answer is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}" onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit();">
-                            <i class="fas fa-caret-up fa-3x"></i>
-                        </a>
-                        <form id="up-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="POST" style="display:none;">
-                            @csrf
-                            <input type="hidden" name="vote" value="1">
-                        </form>
-
-                        <span class="votes-count">{{ $answer->votes_count }}</span>
-
-                        <a title="This answer is not useful" class="vote-down {{ Auth::guest() ? 'off' : '' }}" onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit();">
-                            <i class="fas fa-caret-down fa-3x"></i>
-                        </a>
-                        <form id="down-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="POST" style="display:none;">
-                            @csrf
-                            <input type="hidden" name="vote" value="-1">
-                        </form>
-                        @can ('accept', $answer)
-                        <a title="Mark this answer as best answer" class="{{ $answer->status }} mt-2" onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();">
-                            <i class="fas fa-check fa-2x"></i>
-                        </a>
-                        <form id="accept-answer-{{ $answer->id }}" action="{{ route('answers.accept', $answer->id) }}" method="POST" style="display:none;">
-                            @csrf
-                        </form>
-                        @else
-                        @if ($answer->is_best)
-                        <a title="The question owner accepted this answer as best answer" class="{{ $answer->status }} mt-2">
-                            <i class="fas fa-check fa-2x"></i>
-                        </a>
-                        @endif
-                        @endcan
-                    </div>
-                    <div class="media-body">
-                        {!! $answer->body_html !!}
-                        <div class="row">
-                            <div class="col-4">
-                                <div class="ml-auto">
-                                    @can("update",$answer)
-                                    <a href="{{ route('questions.answers.edit',[$question->id,$answer->id]) }}" class="btn btn-sm btn-outline-info">編集する</a>
-                                    @endcan
-
-                                    @can("delete",$answer)
-                                    <form class="form-delete" method="post" action="{{ route('questions.answers.destroy',[$question->id,$answer->id]) }}">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('本当に削除しますか?')">削除</button>
-                                    </form>
-                                    @endcan
-                                </div>
-                            </div>
-                            <div class="col-4"></div>
-                            <div class="col-4">
-                                @include('shared._author',[
-                                'model' => $answer,
-                                'label' => 'answered'
-                                ])
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr>
+                 @include('answers._answer')
                 @endforeach
             </div>
         </div>
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -18,56 +18,15 @@
                     @include('layouts._messages')
 
                     @forelse($questions as $question)
-                    <div class="media">
-                        <div class="d-flex flex-column counters">
-                            <div class="vote">　
-                                <!-- 投票数の表示 -->
-                                <strong>{{ $question->votes_count }}</strong>{{ str_plural('vote', $question->votes_count) }}
-                                <!--str_pluralで単数形に変換 -->
-                            </div>
-                            <div class="status {{ $question->status }}">
-                                <strong>{{ $question->answers_count }}</strong>{{ str_plural('answer', $question->answers_count) }}
-                            </div>
-                            <div class="view">
-                                {{ $question->views . " " . str_plural('view', $question->views) }}
-                            </div>
-
-                        </div>
-                        <div class="media-body">
-                            <div class="d-flex align-items-center">
-                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                <div class="ml-auto">
-                                    @can("update",$question)
-                                    <a href="{{ route('questions.edit',$question->id) }}" class="btn btn-sm btn-outline-info">編集する</a>
-                                    @endcan
-
-                                    @can("delete",$question)
-                                    <form class="form-delete" method="post" action="{{ route('questions.destroy',$question->id) }}">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('本当に削除しますか?')">削除</button>
-                                    </form>
-                                    @endcan
-                                </div>
-                            </div>
-                            <p class="lead">
-                                質問者:
-                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                <small class="text-muted">{{ $question->created_date }}</small>
-                            </p>
-                            <div class="excerpt">{{ $question->excerpt(350) }}</div>
-                        </div>
-                    </div>
-                    <hr>
+                    @include('questions._excerpt')
+                    
                     @empty
                     <div class="alert alert-warning">
                         <strong>申し訳ありません</strong> 現在質問がございません
                     </div>
                     @endforelse
 
-                    <div class="mx-auto">
                         {{ $questions->links() }} <!-- ページネーションでリンクを作成 -->
-                    </div>
                 </div>
             </div>
         </div>
