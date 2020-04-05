3c2c7737017532e3b78f16c2c76fc88d8de1d11b
marumoti <ukn33@yahoo.co.jp>
2020-04-05 15:59:20 +0900

    最初の画面をindexページに飛ばす 質問の答えを評価が高い順に並び替え 回答が無い時の回答欄を非表示 質問がない時のアラートを表示

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -17,7 +17,7 @@
                 <div class="card-body">
                     @include('layouts._messages')
 
-                    @foreach($questions as $question)
+                    @forelse($questions as $question)
                     <div class="media">
                         <div class="d-flex flex-column counters">
                             <div class="vote">　
@@ -59,7 +59,12 @@
                         </div>
                     </div>
                     <hr>
-                    @endforeach
+                    @empty
+                    <div class="alert alert-warning">
+                        <strong>申し訳ありません</strong> 現在質問がございません
+                    </div>
+                    @endforelse
+
                     <div class="mx-auto">
                         {{ $questions->links() }} <!-- ページネーションでリンクを作成 -->
                     </div>
